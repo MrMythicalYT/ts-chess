b7bd524e4c556ba93e4f3549d57b1018b63f096c
MrMythical <91077061+MrMythicalYT@users.noreply.github.com>
2022-07-03 16:42:41 -0400

    tests

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-// TODO: create index.ts
+// tests
+
+import readline from "readline";
+import { Board, Side } from "./exports";
+
+const l = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+l.question("Enter an FEN\n", (answer) => {
+    const board = Board.fromFEN(answer);
+    // console.log(board.getPiece(6, 1)?.unlegalizedMoves)
+    // board.getPiece(7, 1)?.legalizedMoves
+    board.getPiece(7, 1)?.move([5, 0]);
+    // board.inCheck({ side: Side.White })
+    console.log(board.toVisual());
+});
