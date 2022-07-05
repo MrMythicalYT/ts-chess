437d9fd5e9f91bd8029b1fb2af76da582122961f
MrMythical <91077061+MrMythicalYT@users.noreply.github.com>
2022-07-04 21:18:50 -0400

    added LocationResolvable and Coordinates

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,9 @@ l.question("Enter an FEN\n", (answer) => {
     const board = Board.fromFEN(answer);
     // console.log(board.getPiece(6, 1)?.unlegalizedMoves)
     // board.getPiece(7, 1)?.legalizedMoves
-    board.getPiece(7, 1)?.move([5, 0]);
-    // board.inCheck({ side: Side.White })
+    board.getPiece("e2")?.move("e4");
     console.log(board.toVisual());
+    // board.inCheck({ side: Side.White })
+    // console.log(board.toFEN())
+    // console.log(board.toVisual());
 });
